68cf7819e1f798894ec64c059ec3060e3332dfaa
fruvesito <raul.15.94@hotmail.com>
2023-02-21 21:15:07 +0100

    21022023

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import ResponsiveAppBar from "./components/navbar"
 import Footer__Web from "./components/footer"
+import Info__Web from "./components/info"
 
 function App() {
   return (
     <>
       <ResponsiveAppBar/>
+      <Info__Web />
       <Footer__Web/>
     </>
   )
